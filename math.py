df5edf5b62dcdd5a6cf94606119ac9841edafae5
atharva311 <89286861+atharva311@users.noreply.github.com>
2021-09-03 14:13:50 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implementation 
+def add(x,y):
+    return x+y
+#Subtract implementation
+def sub(x,y):
+    return x-y     #on master branch
+#Multiply implementaion
+def add(x,y):
+    return x*y     #on Bug456 branch
+#Divide implementation
+<<<<<<< HEAD
+def add(x,y):
+    if y==0:        #on master branch
+        return Divide_by_zero_error  
+    else:
+        return x/y
+# square implementaion
+def square(x):
+    return x*x
